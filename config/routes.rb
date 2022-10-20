eef3a93724aec59252ea36ddfa55d20b31f6768c
MoritzWittwer <114660104+MoritzWittwer@users.noreply.github.com>
2022-10-20 18:17:20 +0200

    some modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#show'
+  get '/locations/morioka', to: 'pages#show', as: 'morioka'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
