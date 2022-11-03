3ca69becb734977000674de93009b45ba5082cec
MoritzWittwer <114660104+MoritzWittwer@users.noreply.github.com>
2022-11-03 19:55:31 +0100

    redirected the Landing Page to a Rnd Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
-  before_action :hidden_activities
-  before_action :hidden_location_activities
+  # before_action :hidden_activities
+  # before_action :hidden_location_activities
+  # before_action :hidden_articles # hides the original articles from the moroika Page
   # before_action :set_activity, only: %i[show]
 
   def show
-    random_page
-
-    # hidden_articles unless @location == 19 # TODO: how do i hide articles on a specific location id
+    # random_page #shows a random Page without articles
+    redirect_to location_path(Location.ids.sample) # redirects to a random location
   end
 
   private
@@ -15,15 +15,15 @@ class PagesController < ApplicationController
     @location = Location.all.sample
   end
 
-  def hidden_articles
-    @hide_articles = true
-  end
+  # def hidden_articles
+  #   @hide_articles = true
+  # end
 
-  def hidden_activities
-    @hide_activities = true
-  end
+  # def hidden_activities
+  #   @hide_activities = true
+  # end
 
-  def hidden_location_activities
-    @hide_location_activities = true
-  end
+  # def hidden_location_activities
+  #   @hide_location_activities = true
+  # end
 end
